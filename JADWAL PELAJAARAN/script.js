1c90a66a62071792be1a8b8b67a4e0c9cfc42ca8
regannardiyanta1909-sudo <regannardiyanta.1909@gmail.com>
2025-09-06 02:53:08 +0700

    Add files via upload

---
+++ JADWAL PELAJAARAN/script.js
@@ -0,0 +1,6 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika menu di click
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
